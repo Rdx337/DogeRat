4148a615e4353773e0625377905be57a0f3820fb
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2024-06-22 00:45:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = 7384430884:AAEr955MnrLyXRI7Gjac_Lw0IslTCUV7Jy8
+const id = 7478380002
 const address = 'https://www.google.com'
 
 const app = express();
